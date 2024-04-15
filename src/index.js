b4e8049680bffefd6443a6baec6628ac840a6076
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-04-15 12:50:40 -0300

    Change class inheritance to function composition

--- src/index.js
+++ src/index.js
@@ -1,95 +1,81 @@
-class Entry {
-    constructor (name) {
-        this.name = name;
-        this.generateId();
-    }
-
-    generateId() {
-        this.id = Math.random().toString(36).substring(2) +
-            (new Date()).getTime().toString(36);
+function entryProperties() {
+    return {
+        generateId: () => {
+            return Math.random().toString(36).substring(2) +
+                (new Date()).getTime().toString(36);
+            
+        },
     }
 }
 
+function taskProperties() {
+    return {
+        ...entryProperties,
 
+        setCompleted: () => {
+            this.complete = true;
+        },
 
-class Task extends Entry {
-    
-    setCompleted() {
-        this.complete = true;
-    }
+        setProject: () => {
+            this.project = 'project set';
+        },
 
-    setProject() {
-        this.project = 'project set';
-    }
+        setDate: () => {
+            this.date = 'date set';
+        },
 
-    setDate() {
-        this.date = 'date set';
-    }
-    
-    setDescription() {
-        this.description = 'description set';
-    }
-
-
-}
-
-class Project extends Entry {
-
-}
-
-
-
-
-class List {
-    constructor () {
-        this.list = [];
-    }
-
-    showList = () => {
-        console.log(this.list); 
-    }
-
-    addToList = (entry) => {
-        this.list.push(entry);
-    } 
-
-}
-
-class TaskList extends List {
-    createTask = () => {
-        let name = `Task ${this.list.length+1}`;
-        let newTask = new Task(name);
-        this.addToList(newTask);
-        console.log(newTask);
+        setDescription: () => {
+            this.description = 'description set';
+        },
     }
 }
 
-class ProjectList extends List {
-    createProject = () => {
-        let name = `Project ${this.list.length+1}`;
-        let newProject = new Project(name);
-        this.addToList(newProject);
-        console.log(newProject);
+function projectProperties() {
+    return {
+        ...entryProperties,
     }
 }
 
+function listProperties() {
+    return {
+        showList: () => {
+            console.log(this.list);
+        },
 
+        addToList: (entry) => {
+            this.list.push(entry);
+        },
+    }
+}
 
+function createTask (name) {
+    return {
+        ...taskProperties,
+        name,
+        id: entryProperties().generateId(),
+    }
+}
 
-const taskList = new TaskList();
-const projectList = new ProjectList();
+function createProject (name) {
+    return {
+        ...projectProperties,
+        name,
+        id: entryProperties().generateId(),
+    }
+}
 
+function createTaskList() {
+    return {
+        ...listProperties(),
+        createTask: (t) => {
+        },
+    }
+}
 
-
-
-
-// ------------------------------------------------------------ FUNÇAO PRA DELETAR=============================================
-// function deleteBookFromLibrary(row) {
-//     let bookId = row.querySelector('input').value;
-//     let bookIndex = myLibrary.findIndex(book => {
-//         return book.id === bookId;
-//     });
-
-//     myLibrary.splice(bookIndex, 1);
-//     booksTable.deleteRow(row.rowIndex);
-// }-------------------------------------------------------------------------------------------------------------------------------
+function createProjectList() {
+    return {
+        ...listProperties(),
+        createProject: (p) => {
+        },
+    }
+}
